6af14564ddab4495fa2b67563b29bb3eace62ebf
Sujit <sujitprabhu1912@gmail.com>
2026-07-27 13:54:45 +0530

    Create 1512-Number of Good Pairs.java

---
+++ 1512-Number of Good Pairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] counts = new int[101];
+        int goodPairs = 0;
+        for (int num : nums) {
+            goodPairs += counts[num];
+            counts[num]++;
+        }
+        return goodPairs;
+    }
+}
